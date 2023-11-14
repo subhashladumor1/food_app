8aae11a3e1d5946dcc789f35016715badf013773
Subhash Ladumor <79623012+subhashladumor1@users.noreply.github.com>
2023-11-14 18:11:20 +0530

    image slider p1
    image slider p1

---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors {
+  static final Color textColor = const Color(0xFFccc7c5);
+  static final Color mainColor = const Color(0xFF89dad0);
+  static final Color iconColor1 = const Color(0xFFffd28d);
+  static final Color iconColor2 = const Color(0xFFfaab88);
+  static final Color pargColor = const Color(0xFF8f837f);
+  static final Color buttonBackgroundColor = Color.fromARGB(0, 240, 240, 240);
+  static final Color signColor = const Color(0xFFa9a29f);
+  static final Color titleColor = const Color(0xFF5c524f);
+  static final Color mainBlackColor = const Color(0xFF332d2b);
+  static final Color yellowColor = const Color(0xFFffd379);
+}
